4944dde6bf0b5c4bd3e256ca68c3ad50dfe04cd1
Terry <tripletdawg111@gmail.com>
2018-01-14 19:36:25 +0200

    Add comments for knowledge sharing purposes.

--- pandasreadfromweb.py
+++ pandasreadfromweb.py
@@ -4,14 +4,20 @@ import pandas_datareader.data as web
 import matplotlib.pyplot as plt
 from matplotlib import style
 
-style.use('seaborn')
+style.use('seaborn') # the style to use for graph plotting
 
 start = datetime.datetime(2017, 1, 1)
 end = datetime.datetime.now()
 
+# You will need to find your own API to pull finance data from
 df = web.DataReader("DIVTRX", "google", start, end)
 
-print(df.tail(10))
-df[['Open', 'Close']].plot()
-plt.legend()
-plt.show()
+# Print the dataframe to console.
+#print(df)
+print(df.head()) # prints the first 5 rows
+#print(df.tail(10)) # prints the last 10 rows
+
+# Plot the graph using the data from the dataframe
+#df[['Open', 'Close']].plot() # list the columns to plot
+#plt.legend() # display the legend
+#plt.show() # display the graph on screen
